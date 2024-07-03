0c098df212f9cf5d5331b0a6d59e6c91c38dd627
Marcus Mesquita <marcus.mesquita@totvs.com.br>
2024-07-03 15:24:21 -0300

    Adjust S3 access policy

--- main.tf
+++ main.tf
@@ -25,13 +25,30 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_ss_encrypt
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
+resource "aws_s3_bucket_ownership_controls" "controls" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.bucket.bucket
 
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.controls,
+    aws_s3_bucket_public_access_block.public_access,
+  ]
+
+  bucket = aws_s3_bucket.bucket
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
